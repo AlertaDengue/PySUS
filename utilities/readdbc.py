37820a7d9dcd9b3f28e5caa9b509439066ed21e4
Flávio Codeço Coelho <fccoelho@gmail.com>
2016-08-17 10:01:24 -0300

    Finally figured out how to call the C code using CFFI.

--- utilities/readdbc.py
+++ utilities/readdbc.py
@@ -7,16 +7,14 @@ import os
 import _readdbc as rdbc
 from _readdbc import ffi, lib
 
-print(dir(lib))
-print(dir(ffi))
-print(dir(rdbc))
-
 def dbc2dbf(infile, outfile):
-    infile = ffi.addressof(ffi.new('char[]', os.path.abspath(infile)))
-    outfile = ffi.addressof(ffi.new('char[]', os.path.abspath(outfile)))
-    lib.dbc2dbf(infile, outfile)
+    p = ffi.new('char[]', os.path.abspath(infile))
+    q = ffi.new('char[]', os.path.abspath(outfile))
+
+    lib.dbc2dbf([p], [q])
+
     print(os.path.exists(outfile))
 
 if __name__ == "__main__":
-    dbc2dbf(b'/tmp/DNRJ2014.dbc', b'test.dbf')
+    dbc2dbf(b'/tmp/DNRJ2014.dbc', b'/tmp/output.dbf')
 
