dbefcfad353ae3ff56d9d88eb529edf6eaec2394
Flavio C. Coelho <fccoelho@gmail.com>
2016-08-18 17:05:22 -0300

    fixed bug in the CFFI build
    This closes #1
    
    added readme.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# PySUS
+
+This package collects a set of utilities for handling with public databases published by Brazil's DATASUS
+
+## Features
+
+- Decode encoded patient age to any time unit (years, months, etc)
+- Convert `.dbc` files to DBF databases or read them into pandas dataframes. DBC files are basically DBFs compressed by a proprietary algorithm.
+
+## Instalation
+
+`
+$ sudo pip install PySUS`
--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    ld = f.read()
+
 setup(
     name='PySUS',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     package_data={
-        '': ['*.c', '*.h']
+        '': ['*.c', '*.h', '*.o', '*.so']
     },
-    zip_safe = False,
+    zip_safe=False,
     url='',
     license='gpl-v3',
     author='Flavio Codeco Coelho',
     author_email='fccoelho@gmail.com',
     description="Tools for dealing with Brazil's Public health data",
+    long_description=ld,
     setup_requires=['cffi>=1.0.0'],
     cffi_modules=["pysus/utilities/_build_readdbc.py:ffibuilder"],
-
     install_requires=['pandas', 'dbfread', 'cffi>=1.0.0']
 )
