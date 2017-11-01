4e1ca0590dc2b5e06ed4fcdeb4cd509af03a75a0
Flávio Codeço Coelho <fccoelho@gmail.com>
2017-11-01 16:47:35 -0200

    Added support to download older sinasc data (before 1996)

--- pysus/preprocessing/sinasc.py
+++ pysus/preprocessing/sinasc.py
@@ -10,14 +10,24 @@ from pysus.utilities.readdbc import read_dbc
 
 
 def download(state, year):
+    """
+    Downloads data directly from Datasus ftp server
+    :param state: two-letter state identifier: MG == Minas Gerais
+    :param year: 4 digit integer
+    :return: pandas dataframe
+    """
+    if year < 1994:
+        raise ValueError("SINASC does not contain data before 1994")
     ftp = FTP('ftp.datasus.gov.br')
     ftp.login()
     if year >= 1996:
         ftp.cwd('/dissemin/publicos/SINASC/NOV/DNRES')
+        fname = 'DN{}{}.DBC'.format(state, year)
     else:
         ftp.cwd('/dissemin/publicos/SINASC/ANT/DNRES')
+        fname = 'DNR{}{}.DBC'.format(state, str(year)[-2:])
 
-    fname = 'DN{}{}.DBC'.format(state, year)
+
     ftp.retrbinary('RETR {}'.format(fname), open(fname, 'wb').write)
     df = read_dbc(fname, encoding='iso-8859-1')
     os.unlink(fname)
--- pysus/tests/test_sinasc.py
+++ pysus/tests/test_sinasc.py
@@ -4,9 +4,14 @@ import unittest
 from pysus.preprocessing.sinasc import download
 
 class TestDownload(unittest.TestCase):
-    def test_download(self):
+    def test_download_new(self):
         df = download('CE', 2015)
         self.assertIn('IDADEMAE', df.columns)
+        self.assertGreater(len(df), 0)
+
+    def test_download_old(self):
+        df = download('MG', 1994)
+        self.assertIn('IDADE_MAE', df.columns)
         self.assertGreater(len(df), 0)
 
 
