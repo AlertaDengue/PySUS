8ec7cdd04a2bfd531fa3bc67ce444a7e95c7392b
Flávio Codeço Coelho <fccoelho@gmail.com>
2018-09-20 18:15:07 -0300

    updated travis configuration

--- pysus/tests/test_sinasc.py
+++ pysus/tests/test_sinasc.py
@@ -5,12 +5,12 @@ from pysus.preprocessing.sinasc import download
 
 class TestDownload(unittest.TestCase):
     def test_download_new(self):
-        df = download('CE', 2015)
+        df = download('SE', 2015)
         self.assertIn('IDADEMAE', df.columns)
         self.assertGreater(len(df), 0)
 
     def test_download_old(self):
-        df = download('MG', 1994)
+        df = download('AL', 1994)
         self.assertIn('IDADE_MAE', df.columns)
         self.assertGreater(len(df), 0)
 
