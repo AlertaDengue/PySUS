adf3f81af759c292b58dda997a17a00891e527de
Flávio Codeço Coelho <fccoelho@gmail.com>
2018-09-20 17:27:28 -0300

    updated travis configuration

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ Instalation
 
 setup(
     name='PySUS',
-    version='0.1.13',
+    version='0.2.0',
     packages=find_packages(),
     package_data={
         '': ['*.c', '*.h', '*.o', '*.so', '*.md', '*.txt']
