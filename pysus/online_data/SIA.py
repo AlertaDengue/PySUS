629779374a036d2f01adbb11ceb466ea9baafb02
lucaspcmr <lucaspcmr@gmail.com>
2019-05-09 09:33:27 -0300

    Add files via upload

--- pysus/online_data/SIA.py
+++ pysus/online_data/SIA.py
@@ -12,6 +12,49 @@ from dbfread import DBF
 import pandas as pd
 from pysus.online_data import CACHEPATH
 
+def create_parquet(state: str, year: int, month: int, cache: bool =True):
+    """
+    Download SIH records for state year and month and create cache 
+    :param month: 1 to 12
+    :param state: 2 letter state code
+    :param year: 4 digit integer
+    """
+    state = state.upper()
+    year2 = str(year)[-2:]
+    month = str(month).zfill(2)
+    if year < 1992:
+        raise ValueError("SIH does not contain data before 1994")
+    ftp = FTP('ftp.datasus.gov.br')
+    ftp.login()
+    if year < 2008 and year > 1994:
+        ftype = 'DBC'
+        ftp.cwd('/dissemin/publicos/SIASUS/199407_200712/Dados')
+        fname = 'PA{}{}{}.dbc'.format(state, year2, month)
+        fname2 = None
+    if year >= 2008:
+        ftype = 'DBC'
+        ftp.cwd('/dissemin/publicos/SIASUS/200801_/Dados'.format(year))
+        fname = 'PA{}{}{}.dbc'.format(state, str(year2).zfill(2), month)
+        fname2 = 'BI{}{}{}.dbc'.format(state, str(year2).zfill(2), month)
+    # Check in Cache
+    cachefile = os.path.join('/dados/SIA/', 'SIA_' + fname.split('.')[0] + '_.parquet')
+    cachefile2 = os.path.join('/dados/SIA/', 'SIA_' + fname2.split('.')[0] + '_.parquet')
+    if not cache:
+        df = _fetch_file(fname, ftp, ftype)
+        df.to_parquet(cachefile)
+        df = None
+        df = _fetch_file(fname2, ftp, ftype)
+        df.to_parquet(cachefile2)
+        df = None
+    else:
+        if not os.path.exists(cachefile):
+            df = _fetch_file(fname, ftp, ftype)
+            df.to_parquet(cachefile)
+            df = None
+        if not os.path.exists(cachefile2):
+            df = _fetch_file(fname2, ftp, ftype)
+            df.to_parquet(cachefile2)
+            df = None
 
 def download(state: str, year: int, month: int, cache: bool =True) -> object:
     """
--- pysus/online_data/SIH.py
+++ pysus/online_data/SIH.py
@@ -12,6 +12,39 @@ from pysus.online_data import CACHEPATH
 from dbfread import DBF
 import pandas as pd
 
+def create_parquet(state: str, year: int, month: int, cache: bool=True, tipo_dado='RD') -> object:
+    """
+    Download SIH records for state year and month and returns dataframe
+    :param month: 1 to 12
+    :param state: 2 letter state code
+    :param year: 4 digit integer
+    :param type: tipo (RD, RJ, ER)
+    """
+    state = state.upper()
+    year2 = int(str(year)[-2:])
+    month = str(month).zfill(2)
+    if year < 1992:
+        raise ValueError("SIH does not contain data before 1994")
+    ftp = FTP('ftp.datasus.gov.br')
+    ftp.login()
+    if year < 2008:
+        ftype = 'DBC'
+        ftp.cwd('/dissemin/publicos/SIHSUS/199201_200712/Dados')
+        fname = '{}{}{}{}.dbc'.format(tipo_dado, state, year2, month)
+    if year >= 2008:
+        ftype = 'DBC'
+        ftp.cwd('/dissemin/publicos/SIHSUS/200801_/Dados'.format(year))
+        fname = '{}{}{}{}.dbc'.format(tipo_dado, state, str(year2).zfill(2), month)
+    cachefile = os.path.join('/dados/SIH/', 'SIH_' + fname.split('.')[0] + '_.parquet')
+    if not cache:
+        df = _fetch_file(fname, ftp, ftype)
+        df.to_parquet(cachefile)
+        df = None
+    else:
+        if not os.path.exists(cachefile):
+            df = _fetch_file(fname, ftp, ftype)
+            df.to_parquet(cachefile)
+            df = None
 
 def download(state: str, year: int, month: int, cache: bool=True, tipo_dado='RD') -> object:
     """
