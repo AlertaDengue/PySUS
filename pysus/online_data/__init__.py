8241f38d49b15625dd792db210f0a15c733ba516
Flavio C. Coelho <fccoelho@gmail.com>
2019-11-27 08:55:32 -0300

    fixed bug with checking cachepath

--- pysus/online_data/__init__.py
+++ pysus/online_data/__init__.py
@@ -7,10 +7,10 @@ import os
 from pathlib import Path
 
 # create pysus cache directory
-if not os.path.exists(os.path.join(Path.home(), 'pysus')):
-    os.mkdir(os.path.join(Path.home(), 'pysus'))
+if not os.path.exists(os.path.join(str(Path.home()), 'pysus')):
+    os.mkdir(os.path.join(str(Path.home()), 'pysus'))
 
-CACHEPATH = os.path.join(Path.home(), 'pysus')
+CACHEPATH = os.path.join(str(Path.home()), 'pysus')
 
 
 def cache_contents():
