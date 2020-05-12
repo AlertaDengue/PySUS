afbf999f034aefa47c981839ca3f2a362a72a8de
Flávio Codeço Coelho <fccoelho@gmail.com>
2020-05-12 15:15:45 -0300

    Changed setup.py to try to fix renaming of extension module

--- setup.py
+++ setup.py
@@ -55,8 +55,7 @@ class PostInstall(install):
         atexit.register(_post_install)
         install.run(self)
 
-setuptools_path = setuptools.__path__[0].replace('/setuptools', '')
-pysus_path = setuptools.__path__[0].replace('setuptools', 'pysus')
+
 setup(
     name='PySUS',
     version='0.4.5',
@@ -74,6 +73,32 @@ setup(
     setup_requires=['cffi>=1.0.0', 'setuptools>26.0.0'],
     cffi_modules=["pysus/utilities/_build_readdbc.py:ffibuilder"],
     install_requires=['pandas', 'dbfread', 'cffi>=1.0.0', 'geocoder', 'requests', 'pyarrow', 'fastparquet'],
-    cmdclass={'install': PostInstall},
+    # cmdclass={'install': PostInstall},
 )
 
+
+def _post_install():
+    setuptools_path = setuptools.__path__[0].replace('/setuptools', '')
+    pysus_path = setuptools.__path__[0].replace('setuptools', 'pysus')
+
+    ## This code is to make sure the compiled extension module inside the
+    # utilities module is named _readdbc.so
+    if os.path.exists(pysus_path):
+        comp_mod_list = glob.glob(os.path.join(pysus_path, 'utilities', '_readdbc*.so'))
+        for mod in comp_mod_list:
+            mod = os.path.split(mod)[-1]
+            if mod != '_readdbc.so':
+                print(f'Renaming {mod} to _readdbc.so')
+                os.chdir(os.path.join(pysus_path, 'utilities'))
+                os.rename(mod, '_readdbc.so')
+    # Sometimes the compiled module is thrown on the (site|dist)-packages directory
+    try:
+        shutil.copy(os.path.join(setuptools_path, '_readdbc.abi3.so'),
+                    os.path.join(setuptools_path, 'pysus', 'utilities', '_readdbc.abi3.so')
+                    )
+        print(f'Copied _readdbc.abi3.so to pysus/utilities')
+    except:
+        pass
+
+
+atexit.register(_post_install)
