3246458230c831ee3e7a025fc6df2aaaa037bcd4
Luã Bida Vacaro <luabidaa@gmail.com>
2023-02-03 11:26:00 -0300

    feat(semantic-release): add semantic release to the project

--- pysus/__init__.py
+++ pysus/__init__.py
@@ -1,1 +1,16 @@
+# type: ignore[attr-defined]
+"""PySUS Python package"""
+
 from . import preprocessing, utilities
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "0.6.4"  # changed by semantic-release
+
+
+version: str = get_version()
+__version__: str = version
