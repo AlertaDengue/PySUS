c1daff82c60ae3ad71a028e5485678bc4b88bb1f
Flávio Codeço Coelho <fccoelho@gmail.com>
2023-05-02 13:25:01 -0300

    Revert "chore(translation): pt_br sphinx translation"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,10 +72,7 @@ release = "0.1.13"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'en'
-
-locale_dirs = ['locale/']
-gettext_compact = False 
+language = None
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
