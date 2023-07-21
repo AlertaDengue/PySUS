1f9ae9287bd717066e751ad40ad24966e940a9f7
Luã Bida Vacaro <luabidaa@gmail.com>
2023-07-21 08:29:07 -0300

    Saving work

--- pysus/ftp/databases.py
+++ pysus/ftp/databases.py
@@ -250,6 +250,10 @@ class SINASC(Database):
         source="http://sinasc.saude.gov.br/",
         description="",
     )
+    groups = dict(
+        DN="Declarações de Nascidos Vivos",
+        DNR="Dados dos Nascidos Vivos por UF de residência",
+    )
 
     def describe(self, file: File) -> dict:
         uf, year = self.format(file)
@@ -288,7 +292,7 @@ class SINASC(Database):
             if len(to_list(ufs)) == 1:
                 return []
             else:
-                ufs = to_list(ufs).remove("EX")
+                to_list(ufs).remove("EX")
 
         ufs = parse_UFs(ufs)
         years = [str(y)[-2:].zfill(2) for y in to_list(years)]
@@ -315,7 +319,7 @@ class SINASC(Database):
 
 
 class SIH(Database):
-    name = "SINASC"
+    name = "SIH"
     paths = [
         "/dissemin/publicos/SIHSUS/199201_200712/Dados",
         "/dissemin/publicos/SIHSUS/200801_/Dados",
@@ -329,7 +333,7 @@ class SIH(Database):
         description=(
             "A finalidade do AIH (Sistema SIHSUS) é a de transcrever todos os "
             "atendimentos que provenientes de internações hospitalares que "
-            "foram financiadas pelo sus, e após o processamento, gerarem "
+            "foram financiadas pelo SUS, e após o processamento, gerarem "
             "relatórios para os gestores que lhes possibilitem fazer os pagamentos "
             "dos estabelecimentos de saúde. Além disso, o nível Federal recebe "
             "mensalmente uma base de dados de todas as internações autorizadas "
@@ -346,6 +350,103 @@ class SIH(Database):
         SP="Serviços Profissionais",
         CH="Cadastro Hospitalar",
         CM="",  # TODO
+    )
+
+    def describe(self, file: File) -> dict:
+        if file.extension.upper() == ".DBC":
+            group, uf, year, month = self.format(file)
+
+            description = dict(
+                name=file.basename,
+                group=self.groups[group],
+                uf=UFs[uf],
+                month=MONTHS[int(month)],
+                year=zfill_year(year),
+                size=humanize.naturalsize(file.size),
+                last_update=file.date,
+            )
+
+            return description
+
+    def format(self, file: File) -> tuple:
+        group, uf = file.name[:2].upper(), file.name[2:4].upper()
+        year, month = file.name[-4:-2], file.name[-2:]
+        return group, uf, year, month
+
+    def get_files(
+        self,
+        groups: Union[List[str], str],
+        ufs: Union[List[str], str],
+        months: Union[list, str, int],
+        years: Union[list, str, int],
+    ) -> List[File]:
+        groups = [gr.upper() for gr in to_list(groups)]
+        ufs = parse_UFs(ufs)
+        months = [str(y)[-2:].zfill(2) for y in to_list(months)]
+        years = [str(m)[-2:].zfill(2) for m in to_list(years)]
+
+        if not all([gr in list(self.groups) for gr in groups]):
+            raise ValueError(
+                f"Unknown SIH Group(s): {set(groups).difference(list(self.groups))}"
+            )
+
+        # Fist filter files by group to reduce the files list length
+        groups_files = []
+        for file in self.files:
+            if file.name[:2] in groups:
+                groups_files.append(file)
+
+        targets = ["".join(t) for t in product(ufs, months, years)]
+
+        files = []
+        for file in groups_files:
+            if file.name[2:] in targets:
+                files.append(file)
+
+        return files
+
+
+class SIA(Database):
+    name = "SIA"
+    paths = [
+        "/dissemin/publicos/SIASUS/199407_200712/Dados",
+        "/dissemin/publicos/SIASUS/200801_/Dados",
+    ]
+    metadata = dict(
+        long_name="Sistema de Informações Ambulatoriais",
+        source="http://sia.datasus.gov.br/principal/index.php",
+        description=(
+            "O Sistema de Informação Ambulatorial (SIA) foi instituído pela Portaria "
+            "GM/MS n.º 896 de 29 de junho de 1990. Originalmente, o SIA foi concebido "
+            "a partir do projeto SICAPS (Sistema de Informação e Controle Ambulatorial "
+            "da Previdência Social), em que os conceitos, os objetivos e as diretrizes "
+            "criados para o desenvolvimento do SICAPS foram extremamente importantes e "
+            "amplamente utilizados para o desenvolvimento do SIA, tais como: (i) o "
+            "acompanhamento das programações físicas e orçamentárias; (ii) o "
+            "acompanhamento das ações de saúde produzidas; (iii) a agilização do "
+            "pagamento e controle orçamentário e financeiro; e (iv) a formação de "
+            "banco de dados para contribuir com a construção do SUS."
+        ),
+    )
+    groups = dict(
+        AB="Laudo de Acompanhamento a Cirurgia Bariátrica",
+        ABO="Acompanhamento Pós Cirurgia Bariátrica",
+        ACF="Confeção de Fístula Arteriovenosa",
+        AD="Laudos Diversos",
+        AM="Laudo de Medicamentos",
+        AMP="",  # TODO: find out what AMP means
+        AN="Laudo de Nefrologia",
+        AQ="Laudo de Quimioterapia",
+        AR="Laudo de Radioterapia",
+        ATD="Tratamento Dialítico",
+        BI="Boletim Individual",
+        IMPBO="",  # TODO: same here
+        PA="Produção Ambulatorial",
+        PAM="",  # TODO: same here
+        PAR="",
+        PAS="",
+        PS="Psicossocial",
+        SAD="Atenção Domiciliar",
     )
 
     def describe(self, file: File) -> dict:
