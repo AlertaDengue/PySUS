114b85864ca5f78526a487825a0d204967c48308
Luã Bida Vacaro <luabidaa@gmail.com>
2023-08-21 13:56:31 -0300

    minor fix on zfill_year

--- pysus/ftp/databases.py
+++ pysus/ftp/databases.py
@@ -16,7 +16,7 @@ def zfill_year(year: Union[str, int]) -> int:
     Formats a len(2) year into len(4) with the correct year preffix
     E.g: 20 -> 2020; 99 -> 1999
     """
-    year = str(year)[-2:]
+    year = str(year)[-2:].zfill(2)
     current_year = str(datetime.datetime.now().year)[-2:]
     suffix = "19" if str(year) > current_year else "20"
     return int(suffix + str(year))
