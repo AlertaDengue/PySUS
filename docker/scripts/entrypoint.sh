24c93b5fca06b7d766af532864208c20826530bc
Sandro Loch <es.loch@gmail.com>
2023-12-04 07:55:45 -0300

    Fix broken poetry install

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-exec jupyter-lab --browser='firefox'  --allow-root --NotebookApp.token='' --NotebookApp.password=''
+jupyter_lab_path=$(which jupyter)
+
+if [ -z "$jupyter_lab_path" ]; then
+  echo "Jupyter not found"
+  exit 1
+fi
+
+$jupyter_lab_path lab --browser='firefox' --allow-root --NotebookApp.token='' --NotebookApp.password=''
--- docker/scripts/poetry-install.sh
+++ docker/scripts/poetry-install.sh
@@ -3,4 +3,4 @@
 set -ex
 
 poetry config virtualenvs.create false
-poetry install --all-extras --with docs
+poetry install --without geo
