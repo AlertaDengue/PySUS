c5d2cd862affa7a3d59207782df0326975fc5d68
Luã Bida Vacaro <luabidaa@gmail.com>
2023-12-12 14:23:51 -0300

    Update decode tests

--- pysus/tests/test_decoders.py
+++ pysus/tests/test_decoders.py
@@ -12,7 +12,6 @@ import numpy as np
 import pandas as pd
 from numpy.testing import *
 
-from pysus.online_data import parquets_to_dataframe as to_df
 from pysus.online_data.SIM import download, get_CID10_chapters_table
 from pysus.preprocessing import decoders
 from pysus.preprocessing.SIM import (
@@ -31,7 +30,7 @@ def get_CID10_code(index, code):
 
 
 class TestDecoder(unittest.TestCase):
-    @pytest.mark.timeout(5)
+    @pytest.mark.timeout(10)
     def test_decodifica_idade_retorna_em_anos(self):
         res = decoders.decodifica_idade_SINAN(4010, unidade="Y")
         self.assertEqual(res, 10)
@@ -42,7 +41,7 @@ class TestDecoder(unittest.TestCase):
         res = decoders.decodifica_idade_SINAN(1480, unidade="Y")
         self.assertAlmostEqual(res, 0.0547, places=3)
 
-    @pytest.mark.timeout(5)
+    @pytest.mark.timeout(10)
     def test_decodifica_lista_idades_retorna_em_anos(self):
         res = decoders.decodifica_idade_SINAN([4010] * 3, unidade="Y")
         assert_array_equal(res, np.array([10] * 3))
@@ -53,7 +52,7 @@ class TestDecoder(unittest.TestCase):
         res = decoders.decodifica_idade_SINAN([1480] * 5, unidade="Y")
         assert_array_almost_equal(res, np.array([0.0547] * 5), decimal=3)
 
-    @pytest.mark.timeout(5)
+    @pytest.mark.timeout(10)
     def test_decodifica_idade_retorna_em_anos_SIM(self):
         res = decoders.decodifica_idade_SIM(["501"], unidade="Y")
         assert_array_equal(res, np.array([101]))
@@ -68,21 +67,21 @@ class TestDecoder(unittest.TestCase):
         res = decoders.decodifica_idade_SIM(["010"] * 6, unidade="m")
         assert_array_almost_equal(res, np.array([10.0] * 6))
 
-    @pytest.mark.timeout(5)
+    @pytest.mark.timeout(10)
     def test_verifica_geocodigo(self):
         self.assertTrue(decoders.is_valid_geocode(3304557))
 
-    @pytest.mark.skip(reason="This test takes too long")
-    @pytest.mark.timeout(5)
+    @pytest.mark.timeout(10)
     def test_translate_variables(self):
-        df = to_df(download("sp", 2010))
+        df = download(groups="cid10", states="sp", years=2010).to_dataframe()
         df = decoders.translate_variables_SIM(df)
         sex_array = set(df["SEXO"].unique().tolist())
         assert sex_array <= set(["Masculino", "Feminino", "NA"])
         raca_array = set(df["RACACOR"].unique().tolist())
-        assert raca_array <= set(["Branca", "Preta", "Amarela", "nan", "Parda", "Indígena", "NA"])
+        assert raca_array <= set(
+            ["Branca", "Preta", "Amarela", "nan", "Parda", "Indígena", "NA"])
 
-    @pytest.mark.timeout(5)
+    @pytest.mark.timeout(10)
     def test_get_cid_chapter(self):
         code_index = decoders.get_CID10_code_index(get_CID10_chapters_table())
         test_causes = pd.DataFrame(
@@ -104,25 +103,27 @@ class TestDecoder(unittest.TestCase):
                 ]
             }
         )
-        results = test_causes["causas"].map(lambda x: get_CID10_code(code_index, x))
-        assert_array_equal(results, [1, 1, 2, -1, 3, 7, 7, 8, -1, 20, 20, -1, 22])
+        results = test_causes["causas"].map(
+            lambda x: get_CID10_code(code_index, x)
+        )
+        assert_array_equal(
+            results, [1, 1, 2, -1, 3, 7, 7, 8, -1, 20, 20, -1, 22])
 
-    @pytest.mark.skip(reason="This test takes too long")
-    @pytest.mark.timeout(5)
+    @pytest.mark.timeout(10)
     def test_group_and_count(self):
-        df = to_df(download("se", 2010))
+        df = download(groups="cid10", states="se", years=2010).to_dataframe()
         df = decoders.translate_variables_SIM(df)
         variables = ["CODMUNRES", "SEXO", "IDADE_ANOS"]
         counts = group_and_count(df, variables)
         sample = (
-            counts[counts["COUNTS"] != 0]["COUNTS"].sample(20, random_state=0).tolist()
+            counts[counts["COUNTS"] != 0]["COUNTS"].sample(
+                20, random_state=0).tolist()
         )
         self.assertGreater(sum(sample), 0)
 
-    @pytest.mark.skip(reason="This test takes too long")
-    @pytest.mark.timeout(5)
+    @pytest.mark.timeout(10)
     def test_redistribute(self):
-        df = to_df(download("sp", 2010))
+        df = download(groups="cid10", states="sp", years=2010).to_dataframe()
         df = decoders.translate_variables_SIM(
             df, age_classes=True, classify_cid10_chapters=True
         )
@@ -136,16 +137,19 @@ class TestDecoder(unittest.TestCase):
         assert_almost_equal(sum_original, sum_redistributed, 10)
 
         sample = (
-            counts[counts["COUNTS"] != 0]["COUNTS"].sample(20, random_state=0).tolist()
+            counts[counts["COUNTS"] != 0]["COUNTS"].sample(
+                20, random_state=0).tolist()
         )
         assert len(sample) == 20
 
-        counts = redistribute_cid_chapter(counts, ["CODMUNRES", "SEXO", "IDADE_ANOS"])
+        counts = redistribute_cid_chapter(
+            counts, ["CODMUNRES", "SEXO", "IDADE_ANOS"])
         sum_redistributed = counts["COUNTS"].sum()
 
         assert_almost_equal(sum_original, sum_redistributed, 10)
 
         sample = (
-            counts[counts["COUNTS"] != 0]["COUNTS"].sample(20, random_state=0).tolist()
+            counts[counts["COUNTS"] != 0]["COUNTS"].sample(
+                20, random_state=0).tolist()
         )
         assert len(sample) == 20
